74817ff2bf5d5d4eaa669be99be1448ad865d49f
Tim Boeckstaens <boeckstaenstim@gmail.com>
2019-03-24 15:41:22 +0100

    remove TODO that is done

--- src/main/java/be/tim/codingtim/credit/CreditRater.java
+++ src/main/java/be/tim/codingtim/credit/CreditRater.java
@@ -3,6 +3,5 @@ package be.tim.codingtim.credit;
 import be.tim.codingtim.SocialSecurityNumber;
 
 public interface CreditRater {
-    //TODO move to other package
     CreditRating rate(SocialSecurityNumber socialSecurityNumber);
 }
